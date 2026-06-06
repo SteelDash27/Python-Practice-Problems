823f0f9de551cca854ac0d4c85aa5a87cf1f7293
Laone Makeketa <laonemakeketa@gmail.com>
2026-06-06 20:57:05 +0200

    Refactor times table logic and add explanatory documentation

--- (Incomplete)Python Times Table/Python Times Table.py
+++ (Incomplete)Python Times Table/Python Times Table.py
@@ -1,15 +1,18 @@
-
-#n: int
+      
 def times_table():
-    count = 1, times = 1
-    num = int(input("Enter in a number"))
+    row = 1
+    times = 1
+    num = int(input("Enter in a number: "))
+    widthmax = len((str)(num*num))
 
-    while(times <= num): 
-        for x in range(count*times, num+1):
-            print(count*times, end = " ")
-            if((count % num) == 0):
-                print()
-            count= count +1
+    for i in range(row,num+1):#outer for loop
+        for j in range(1,num+1):
+            #print(j*times, end = " ")
+            if(j < 10):
+                print((str)(j*times).rjust(widthmax),end= " ")
+            else:
+                print(j*times, end= "")
+        times = times + 1
         print()
-       
+
 times_table()
