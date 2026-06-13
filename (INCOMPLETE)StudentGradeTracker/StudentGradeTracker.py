22d8e37e47508e2c5b595afc943009a4d1b9e2d0
Laone Makeketa <laonemakeketa@gmail.com>
2026-06-14 00:14:42 +0200

    Implement functionality to calculate maximum, minimum, and average scores, and count scores above average in Student Grade Tracker

--- (INCOMPLETE)StudentGradeTracker/StudentGradeTracker.py
+++ (INCOMPLETE)StudentGradeTracker/StudentGradeTracker.py
@@ -1,23 +1,45 @@
 
 numStudents = int(input("Enter in the number of students: "))
 scores = []
-    
-count = 1
-for i in range(0,numStudents):
-    scores.append(int(input(f"Enter score for student {i+1}: ")))
-    if(scores[i] > (sum(scores)/len(scores))):
+
+def maximum(lst):
+    maxnum = 0
+    for x in range(0,len(lst)):
+        if(lst[x] > maxnum):
+            maxnum = lst[x]
+    print("Highest mark: ", maxnum)
+
+def minimum(lst):
+    minnum = 100
+    for i in range(0, len(lst)):
+        if(lst[i] < minnum):
+            minnum = lst[i]
+    print("Lowest mark: ",minnum)
+
+def average(lst):
+    count = 0
+    sum = 0
+    for z in range(0,len(lst)):
+        sum += lst[z]
         count +=1
-
-print(scores)
-
-def maximum(lis: int):
-    for i in range():
-         
+    return round(float(sum/count),2)
+    #print("Average mark: ",round(sum/count,2))
 
 
+def report_scores(lst):
+    count = 0
+    for i in range(0,numStudents):
+        scores.append(int(input(f"Enter score for student {i+1}: ")))
+        if(scores[i] > average(lst)):
+            count +=1
+    if(count == 0):
+        count = " "
+
+    print("\nMarks: ",scores, "\n")
+    maximum(lst)
+    minimum(lst)
+    average(lst)
+    print("Number of marks above the average: ", count,"\n")
 
 
-print("Highest mark: ",max(scores))
-print("Lowest mark: ",min(scores))
-print("Average mark: ",float(sum(scores)/len(scores)))
-print("Number of marks above the average")
+report_scores(scores)
--- Word Frequency Counter/WordFrequencyCounter.py
+++ Word Frequency Counter/WordFrequencyCounter.py
@@ -1,3 +1,12 @@
 
-def word_frequency(text):
-    
+diction = {}
+def word_frequency(dict):
+    for i in range(0, 3):
+        number = int(input("Enter in a number"))
+        dict.append(number)
+        print(number)
+        print(dict)
+
+word_frequency(diction)
+
+
