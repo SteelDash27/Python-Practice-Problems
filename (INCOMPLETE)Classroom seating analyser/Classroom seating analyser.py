3f32895009bd339044dcd7e09362ef2dcff6074a
Laone Makeketa <laonemakeketa@gmail.com>
2026-06-15 15:25:27 +0200

    Refactor row_averages function to improve readability and structure

--- (INCOMPLETE)Classroom seating analyser/Classroom seating analyser.py
+++ (INCOMPLETE)Classroom seating analyser/Classroom seating analyser.py
@@ -6,6 +6,7 @@ classroom = [
     [91, 84, 77, 89, 93]
 ]
 
+
 def top_students(lst,threshold):
     above = [] #list of scors above threshold
     for row in range(0,len(classroom)):#Takes the number of rows
@@ -14,17 +15,20 @@ def top_students(lst,threshold):
                 above.append(position)
     print(f"Top scores (above {threshold}): ",above)
 
+
 def row_averages(lst):
     average = []
     for row in range(0,len(lst)):
         count = 0
         sum = 0
-        for num in lst[row]:
+        
+        """for num in lst[row]:
             count += 1
             sum += num
-            average.append(round(float(sum/count)),2)
-        
-    print(average)
+            avg = round((float(sum/count)),2)
+            average.append(avg)  
+    print(lst)
+    print(average)"""
 
 
 """threshold = int(input("Enter in a number that acts as the threshold"))
