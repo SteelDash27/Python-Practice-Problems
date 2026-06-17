af55bc6da6d26e2f0cbdaa85413eb300e0e691e4
Laone Makeketa <laonemakeketa@gmail.com>
2026-06-17 13:31:45 +0200

    Add initial implementation and problem statement for Student Leaderboard

--- (INCOMPLETE)Student Leaderboard/Student Leaderboard.py
+++ (INCOMPLETE)Student Leaderboard/Student Leaderboard.py
@@ -29,9 +29,20 @@ def above_average(list):
         if(list[x][1] > average):
             text += str(list[x][0]) + " "
     print(text)
-        
 
+def reverse_leaderboard(list):
+    #temporary variable
+    low_to_hi = list
+    temp = 0
 
+    for i in range(0, len(list)):
+        while(i + 1 != len(list)):
+            if(low_to_hi[i][1] > low_to_hi[i + 1][1]):
+                temp = low_to_hi[i+1][1]
+                low_to_hi[i + 1][1] = low_to_hi[i][1]
+                low_to_hi[i][1] = temp
+    print(low_to_hi)
 
+reverse_leaderboard(students)
 #top_scorers(students)
-above_average(students)
+#above_average(students)
